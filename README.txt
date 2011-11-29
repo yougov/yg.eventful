437c93f5f3197d84e5ebd68a3f769ec88d764055
Jason R. Coombs <jaraco@jaraco.com>
2011-11-29 16:39:32 -0500

    Updated setup.py and added readme to fix warnings during setup

---
+++ README.txt
@@ -0,0 +1,9 @@
+Eventful
+========
+
+A library to facilitate pyevent stuff.
+
+History
+=======
+
+1.0.8 is the last release maintained by jamwt.
--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
-setup(name="eventful",
+setup_params = dict(
+	name="eventful",
 	version="1.0.8",
+	url="http://dev.yougov.com/G",
 	author="Jamie Turner",
+	author_email="dev@yougov.com",
 	packages=["eventful", "eventful.proto"],
-	)
+)
 
+if __name__ == '__main__':
+	setup(**setup_params)
