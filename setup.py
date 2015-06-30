060e520b549e913295fb305b63c44c7301375254
Jason R. Coombs <jaraco@jaraco.com>
2015-06-30 15:45:44 -0400

    Update repository location

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup_params = dict(
 	name="eventful",
 	use_hg_version=True,
-	url="http://dev.yougov.com/G",
+	url="https://yougov.kilnhg.com/Code/Repositories/support/yg-eventful",
 	author="Jamie Turner",
 	author_email="dev@yougov.com",
 	packages=["eventful", "eventful.proto"],
