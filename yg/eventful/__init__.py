49d6d061492c4b03e65f06ae94002b79624bdf1b
Jason R. Coombs <jaraco@jaraco.com>
2015-06-30 15:53:27 -0400

    Move eventful package into yg namespace

---
+++ yg/eventful/__init__.py
@@ -0,0 +1,6 @@
+from eventful.logmod import *
+from eventful.app import Application, Service, Client
+from eventful.protocol import *
+from eventful.timers import call_later, call_every
+from eventful.defer import Deferred
+from eventful.mixins import *
